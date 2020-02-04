7db0f09594fb37dbd41f4dd5b4b3c824c9fa1cd8
adjeteGuru <justinadjev@hotmail.com>
2020-02-04 16:59:39 +0000

    unfinished

---
+++ CloudbassCRUDII/Controllers/BookingCrewDController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CloudbassCRUDII.Models;
+
+namespace CloudbassCRUDII.Controllers
+{
+    public class BookingCrewDController : Controller
+    {
+        private CloudbassDBMSEntities db = new CloudbassDBMSEntities();
+
+        // GET: BookingCrewD
+        public ActionResult Index()
+        {
+            var bookingCrews = db.BookingCrews.Include(b => b.Has_Role).Include(b => b.Schedule);
+            return View(bookingCrews.ToList());
+        }
+
+        // GET: BookingCrewD/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookingCrew bookingCrew = db.BookingCrews.Find(id);
+            if (bookingCrew == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookingCrew);
+        }
+
+        // GET: BookingCrewD/Create
+        public ActionResult Create()
+        {
+            ViewBag.has_RoleId = new SelectList(db.Has_Role, "Id", "Id");
+            ViewBag.scheduleId = new SelectList(db.Schedules, "Id", "text");
+            return View();
+        }
+
+        // POST: BookingCrewD/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "scheduleId,has_RoleId,start_date,end_date,totalDays,rate")] BookingCrew bookingCrew)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BookingCrews.Add(bookingCrew);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.has_RoleId = new SelectList(db.Has_Role, "Id", "Id", bookingCrew.has_RoleId);
+            ViewBag.scheduleId = new SelectList(db.Schedules, "Id", "text", bookingCrew.scheduleId);
+            return View(bookingCrew);
+        }
+
+        // GET: BookingCrewD/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookingCrew bookingCrew = db.BookingCrews.Find(id);
+            if (bookingCrew == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.has_RoleId = new SelectList(db.Has_Role, "Id", "Id", bookingCrew.has_RoleId);
+            ViewBag.scheduleId = new SelectList(db.Schedules, "Id", "text", bookingCrew.scheduleId);
+            return View(bookingCrew);
+        }
+
+        // POST: BookingCrewD/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "scheduleId,has_RoleId,start_date,end_date,totalDays,rate")] BookingCrew bookingCrew)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bookingCrew).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.has_RoleId = new SelectList(db.Has_Role, "Id", "Id", bookingCrew.has_RoleId);
+            ViewBag.scheduleId = new SelectList(db.Schedules, "Id", "text", bookingCrew.scheduleId);
+            return View(bookingCrew);
+        }
+
+        // GET: BookingCrewD/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookingCrew bookingCrew = db.BookingCrews.Find(id);
+            if (bookingCrew == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookingCrew);
+        }
+
+        // POST: BookingCrewD/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BookingCrew bookingCrew = db.BookingCrews.Find(id);
+            db.BookingCrews.Remove(bookingCrew);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CloudbassCRUDII/Controllers/ScheduleDController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CloudbassCRUDII.Models;
+
+namespace CloudbassCRUDII.Controllers
+{
+    public class ScheduleDController : Controller
+    {
+        private CloudbassDBMSEntities db = new CloudbassDBMSEntities();
+
+        // GET: ScheduleD
+        public ActionResult Index()
+        {
+            var schedules = db.Schedules.Include(s => s.Job).Include(s => s.ScheduleStatu).Include(s => s.SchType);
+            return View(schedules.ToList());
+        }
+
+        // GET: ScheduleD/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Schedule schedule = db.Schedules.Find(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedule);
+        }
+
+        // GET: ScheduleD/Create
+        public ActionResult Create()
+        {
+            ViewBag.JobId = new SelectList(db.Jobs, "Id", "text");
+            ViewBag.statusId = new SelectList(db.ScheduleStatus, "Id", "title");
+            ViewBag.SchTypeId = new SelectList(db.SchTypes, "Id", "name");
+            return View();
+        }
+
+        // POST: ScheduleD/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,text,start_date,end_date,SchTypeId,JobId,statusId")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Schedules.Add(schedule);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.JobId = new SelectList(db.Jobs, "Id", "text", schedule.JobId);
+            ViewBag.statusId = new SelectList(db.ScheduleStatus, "Id", "title", schedule.statusId);
+            ViewBag.SchTypeId = new SelectList(db.SchTypes, "Id", "name", schedule.SchTypeId);
+            return View(schedule);
+        }
+
+        // GET: ScheduleD/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Schedule schedule = db.Schedules.Find(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.JobId = new SelectList(db.Jobs, "Id", "text", schedule.JobId);
+            ViewBag.statusId = new SelectList(db.ScheduleStatus, "Id", "title", schedule.statusId);
+            ViewBag.SchTypeId = new SelectList(db.SchTypes, "Id", "name", schedule.SchTypeId);
+            return View(schedule);
+        }
+
+        // POST: ScheduleD/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,text,start_date,end_date,SchTypeId,JobId,statusId")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(schedule).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.JobId = new SelectList(db.Jobs, "Id", "text", schedule.JobId);
+            ViewBag.statusId = new SelectList(db.ScheduleStatus, "Id", "title", schedule.statusId);
+            ViewBag.SchTypeId = new SelectList(db.SchTypes, "Id", "name", schedule.SchTypeId);
+            return View(schedule);
+        }
+
+        // GET: ScheduleD/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Schedule schedule = db.Schedules.Find(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedule);
+        }
+
+        // POST: ScheduleD/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Schedule schedule = db.Schedules.Find(id);
+            db.Schedules.Remove(schedule);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
